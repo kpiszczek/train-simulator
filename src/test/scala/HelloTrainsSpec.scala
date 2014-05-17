1887b48b30e789418788b98dfb09700e03855bef
Kuba Piszczek <jppiszczek@gmail.com>
2014-05-17 23:14:21 +0000

    train simulation

--- src/test/scala/HelloTrainsSpec.scala
+++ src/test/scala/HelloTrainsSpec.scala
@@ -12,23 +12,6 @@ class HelloTrainsSpec(_system: ActorSystem)
   with FlatSpec
   with BeforeAndAfterAll {
 
-  def this() = this(ActorSystem("HelloAkkaSpec"))
+  def this() = this(ActorSystem("HelloTrainsSpec"))
 
-  override def afterAll: Unit = {
-    system.shutdown()
-    system.awaitTermination(10.seconds)
-  }
-
-  "An HelloAkkaActor" should "be able to set a new greeting" in {
-    val greeter = TestActorRef(Props[Greeter])
-    greeter ! WhoToGreet("testkit")
-    greeter.underlyingActor.asInstanceOf[Greeter].greeting should be("hello, testkit")
-  }
-
-  it should "be able to get a new greeting" in {
-    val greeter = system.actorOf(Props[Greeter], "greeter")
-    greeter ! WhoToGreet("testkit")
-    greeter ! Greet
-    expectMsgType[Greeting].message.toString should be("hello, testkit")
-  }
 }
